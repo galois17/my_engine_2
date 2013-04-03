e28895ca486902cd7d967d2e3aedb60741b3bef2
Khem Veasna <kveasna@anypresence.com>
2013-04-02 23:07:13 -0400

    Minor tweak.

---
+++ lib/foo_corp/my_engine_extension/my_engine.rb
@@ -0,0 +1,13 @@
+module FooCorp
+  module MyEngineExtension
+    module MyEngine
+    
+      def setup
+      end
+    
+      def do_foo(object_instance, options={})
+      end
+      
+    end
+  end
+end
--- lib/my_engine_2.rb
+++ lib/my_engine_2.rb
@@ -1,4 +1,5 @@
 require "my_engine_2/engine"
+require "foo_corp/my_engine_extension/my_engine"
 
 module MyEngine2
 end
